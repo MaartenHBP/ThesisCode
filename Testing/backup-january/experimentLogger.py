57fa77c8dcca41cf36abe66e14e3b3f942864fc5
MaartenHBP <maarten.hb.pyck@gmail.com>
2023-04-04 10:29:43 +0200

    further cleaning up

--- Testing/backup-january/experimentLogger.py
+++
@@ -1,7 +0,0 @@
-class ExperimentLogger:
-    def __init__(self, func) -> None:
-        self.func = func
-
-    def log(self, q, maxq):
-        self.func()
-        print("Queries answererd: " + str(q) + "/" + str(maxq), end = "\r")
