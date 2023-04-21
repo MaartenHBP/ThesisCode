15e0b11e8e705bb911e28288cec8ebec86dbcf8b
MaartenHBP <maarten.hb.pyck@gmail.com>
2023-04-21 14:14:34 +0200

    bleh

--- Testing/experiment.py
+++ Testing/experiment.py
@@ -569,7 +569,7 @@ if __name__ == "__main__":
     # make plots
     # doAll(Path(f"experimenten/learnMetric/superinstance1/100").absolute())
 
-    doAll(Path(f"experimenten/rebuild/metric_True/rebuildLevel_superinstance/0/100").absolute())
+    doAll(Path(f"experimenten/rebuild_notall/metric_True/rebuildLevel_all/100").absolute())
 
     # doAll(Path(f"experimenten/rebuild/metric_False/rebuildLevel_superinstance/0/100").absolute())
 
